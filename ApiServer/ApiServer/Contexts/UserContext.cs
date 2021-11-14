f522c21e5a7e86268e69dac36af94347105c8853
NEXT_GM <astashinboris56@gmail.com>
2021-11-14 11:07:26 +0300

    apiserver

---
+++ ApiServer/ApiServer/Contexts/UserContext.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.EntityFrameworkCore;
+using ApiServer.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ApiServer.Contexts
+{
+    public class UserContext : DbContext
+    {
+        public UserContext(DbContextOptions<UserContext> option) : base(option)
+        {
+
+        }
+
+        public DbSet<User> Users { get; set; }
+    }
+}
---
+++ ApiServer/ApiServer/Models/User.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+ namespace ApiServer.Models
+{
+    public class User
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string SurName { get; set; }
+        public string Title { get; set; }
+        public bool isBoy { get; set; }
+    }
+}
